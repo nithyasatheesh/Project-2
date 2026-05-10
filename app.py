6ea66b0463b97385821ca1bfd3f6b63105d272a5
nithyasatheesh <32924400+nithyasatheesh@users.noreply.github.com>
2026-05-10 16:08:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
 
+import pandas as pd
+import streamlit as st
+
+from agents.audio_summary_agent import AudioSummaryAgent
+from agents.case_study_evaluator import CaseStudyEvaluator
+from agents.coding_tutor_agent import CodingTutorAgent
+from agents.quiz_generator_agent import QuizGeneratorAgent
+from agents.summary_agent import SummaryAgent
+from agents.visualization_agent import VisualizationAgent
+
+# ---------------- PAGE CONFIG ---------------- #
+
+st.set_page_config(
+    page_title="Multi-Agent AI Learning Platform",
+    page_icon="💻",
+    layout="wide"
+)
+
+st.title("💻 Multi-Agent AI Technical Learning Platform")
+
+# ---------------- AGENTS ---------------- #
+
+coding_agent = CodingTutorAgent()
+summary_agent = SummaryAgent()
+case_agent = CaseStudyEvaluator()
+quiz_agent = QuizGeneratorAgent()
+visual_agent = VisualizationAgent()
+audio_agent = AudioSummaryAgent()
+
+# ---------------- SIDEBAR ---------------- #
+
+agent_type = st.sidebar.selectbox(
+    "Select Agent",
+    [
+    st.audio(audio_bytes, format="audio/mp3")
